7782902830e162f74e27bfe965b7dd09836d5a6b
Dmitry Chaykin <d.chaykin@setlog.com>
2025-04-10 18:24:15 +0200

    generating uuid

---
+++ helper/uuid.go
@@ -0,0 +1,34 @@
+package helper
+
+import (
+	"crypto/rand"
+	"encoding/hex"
+	"errors"
+	"fmt"
+	"strconv"
+	"time"
+)
+
+func GenerateUUID() (string, error) {
+	timestamp := time.Now().Unix()
+	timestampHex := fmt.Sprintf("%08x", timestamp)
+	randomBytes := make([]byte, 12)
+	_, err := rand.Read(randomBytes)
+	if err != nil {
+		return "", err
+	}
+	randomHex := hex.EncodeToString(randomBytes)
+	return timestampHex + randomHex, nil
+}
+
+func ExtractTimeFromUUID(uuid string) (time.Time, error) {
+	if len(uuid) < 8 {
+		return time.Time{}, errors.New("UUID is too short for fetching the timestamp")
+	}
+	timestampHex := uuid[:8]
+	unixSeconds, err := strconv.ParseInt(timestampHex, 16, 64)
+	if err != nil {
+		return time.Time{}, fmt.Errorf("invalid Hex-Timestamp: %w", err)
+	}
+	return time.Unix(unixSeconds, 0), nil
+}
---
+++ helper/uuid_test.go
@@ -0,0 +1,22 @@
+package helper
+
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestGenerateAndExtractUUID(t *testing.T) {
+	uuid, err := GenerateUUID()
+	require.NoError(t, err)
+	require.Equal(t, 32, len(uuid))
+
+	extractedTime, err := ExtractTimeFromUUID(uuid)
+	require.NoError(t, err)
+
+	now := time.Now()
+	diff := now.Sub(extractedTime)
+	require.LessOrEqual(t, diff, 2*time.Second)
+	require.GreaterOrEqual(t, diff, -2*time.Second)
+}
