33d506c2fbaeab51a9572333e445f1fa6aa9b3fa
Dmitry Chaykin <d.chaykin@setlog.com>
2025-05-22 18:30:07 +0200

    WrapError implemented

---
+++ log/error.go
@@ -0,0 +1,21 @@
+package log
+
+import (
+	"fmt"
+	"runtime"
+)
+
+type errorWithPos struct {
+	File string
+	Line int
+	Err  error
+}
+
+func (e *errorWithPos) Error() string {
+	return fmt.Sprintf("%s:%d: %v", e.File, e.Line, e.Err)
+}
+
+func WrapError(err error) error {
+	_, file, line, _ := runtime.Caller(1)
+	return &errorWithPos{File: file, Line: line, Err: err}
+}
