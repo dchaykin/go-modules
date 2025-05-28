e567297b0b01400563369af75ee07a1194a3c32d
Dmitry Chaykin <d.chaykin@setlog.com>
2025-05-28 12:18:16 +0200

    request-params functions

---
+++ httpcomm/request-params.go
@@ -0,0 +1,13 @@
+package httpcomm
+
+import (
+	"net/http"
+	"strconv"
+)
+
+func GetTenantVersionFromRequest(r *http.Request) (string, int, error) {
+	tenant := r.URL.Query().Get("tenant")
+	versionParam := r.URL.Query().Get("version")
+	version, err := strconv.Atoi(versionParam)
+	return tenant, version, err
+}
