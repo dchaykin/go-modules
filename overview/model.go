f4fbac8a4bab5679843569952d0349beea523cec
Dmitry Chaykin <d.chaykin@setlog.com>
2025-05-30 12:13:29 +0200

    DataRecord.UUID() added

--- overview/model.go
+++ overview/model.go
@@ -6,3 +6,14 @@ type DataRecord struct {
 	Row    map[string]any           `json:"row"`
 	Access []datamodel.AccessConfig `json:"access"`
 }
+
+func (r DataRecord) UUID() string {
+	if r.Row == nil {
+		return ""
+	}
+	uuid, ok := r.Row["uuid"]
+	if !ok {
+		return ""
+	}
+	return uuid.(string)
+}
