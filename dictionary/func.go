81d61ebd32b47cff028aeab6b5dc10065fb826d3
Dmitry Chaykin <d.chaykin@setlog.com>
2025-06-30 12:11:17 +0200

    dictionary package added

---
+++ dictionary/func.go
@@ -0,0 +1,26 @@
+package dictionary
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/dchaykin/go-modules/httpcomm"
+	"github.com/gorilla/mux"
+)
+
+func DownloadByLanguage(w http.ResponseWriter, r *http.Request, path string) {
+	vars := mux.Vars(r)
+	language := vars["language"]
+
+	dictionaryFile := fmt.Sprintf("%s/%s.csv", path, language)
+	content, err := os.ReadFile(dictionaryFile)
+	if err != nil {
+		httpcomm.SetResponseError(&w, "Unable to read the dictionary file: "+dictionaryFile, err, http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "text/csv")
+	w.WriteHeader(http.StatusOK)
+	w.Write(content)
+}
