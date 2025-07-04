37352884c23af9c1d03628db8c41264630e1e8d8
Dmitry Chaykin <d.chaykin@setlog.com>
2025-07-04 20:58:15 +0200

    rebuild overview implemented

--- overview/func.go
+++ overview/func.go
@@ -12,7 +12,22 @@ import (
 	"github.com/dchaykin/go-modules/log"
 )
 
-func ConfigureOverview(userIdentity auth.UserIdentity, tenantConfig datamodel.TenantConfig, tenant string) error {
+func CreateTemporaryOverview(userIdentity auth.UserIdentity, version int, subPath string) error {
+	tenant := userIdentity.Tenant()
+
+	if subPath != "" {
+		subPath += "/"
+	}
+
+	configPath := os.Getenv("ASSETS_PATH") + "config/" + subPath + tenant
+
+	log.Info("Creating overview for tenant %s, role 'default', version %d", tenant, version)
+
+	tenantConfig, err := datamodel.LoadDataModelByRole(configPath, "default", version)
+	if err != nil {
+		return err
+	}
+
 	payload, err := json.Marshal(tenantConfig)
 	if err != nil {
 		return err
@@ -25,6 +40,53 @@ func ConfigureOverview(userIdentity auth.UserIdentity, tenantConfig datamodel.Te
 	}
 
 	log.Info("Overview %s config for tenant %s created, version %d. Response: %s", tenantConfig.Subject, tenant, tenantConfig.Version, string(resp.Answer))
+	return nil
+}
+
+func BulkInsertIntoOverview(userIdentity auth.UserIdentity, subject string, entityList []datamodel.DomainEntity, isTemporary bool) error {
+	recordList := []DataRecord{}
+	for _, entity := range entityList {
+		record := DataRecord{
+			Row:    entity.OverviewRow(),
+			Access: entity.GetAccessConfig(),
+		}
+		recordList = append(recordList, record)
+	}
+
+	log.Info("Bulk inserting %d records into overview for subject '%s'", len(recordList), subject)
+
+	payload, err := json.Marshal(recordList)
+	if err != nil {
+		return err
+	}
+
+	params := map[string]string{}
+	if isTemporary {
+		params["temporary"] = "true"
+	}
+
+	endpoint := fmt.Sprintf("https://%s/app-overview/api/bulk-insert/%s", os.Getenv("MYHOST"), subject)
+	resp := httpcomm.Post(endpoint, userIdentity, params, string(payload))
+	if resp.StatusCode != http.StatusOK {
+		return resp.GetError()
+	}
+
+	log.Info("Bulk insert into overview for subject '%s' completed successfully", subject)
+	return nil
+}
+
+func CommitOverview(userIdentity auth.UserIdentity, subject string) error {
+	tenant := userIdentity.Tenant()
+
+	log.Info("Committing overview for tenant '%s', subject '%s'", tenant, subject)
+
+	endpoint := fmt.Sprintf("https://%s/app-overview/api/commit/overview/%s/%s", os.Getenv("MYHOST"), tenant, subject)
+	resp := httpcomm.Post(endpoint, userIdentity, nil, "")
+	if resp.StatusCode != http.StatusOK {
+		return resp.GetError()
+	}
+
+	log.Info("Overview for tenant '%s', subject '%s' committed successfully", tenant, subject)
 	return nil
 }
 
