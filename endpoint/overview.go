d3e09c06a6c1f3df71b9e44c3d62fa56dea99fbc
Dmitry Chaykin <d.chaykin@setlog.com>
2025-07-04 21:13:21 +0200

    rebuild overview implemented

---
+++ endpoint/overview.go
@@ -0,0 +1,60 @@
+package endpoint
+
+import (
+	"net/http"
+
+	"github.com/dchaykin/go-modules/auth"
+	"github.com/dchaykin/go-modules/datamodel"
+	"github.com/dchaykin/go-modules/httpcomm"
+	"github.com/dchaykin/go-modules/overview"
+)
+
+type OnNextBulkInsert func() ([]datamodel.DomainEntity, error)
+
+func RebuildOverview(w http.ResponseWriter, r *http.Request, subject string, f OnNextBulkInsert) {
+	userIdentity, err := auth.GetUserIdentityFromRequest(*r)
+	if err != nil {
+		httpcomm.SetResponseError(&w, "", err, http.StatusUnauthorized)
+		return
+	}
+
+	_, version, err := GetTenantVersionFromRequest(r)
+	if err != nil {
+		httpcomm.SetResponseError(&w, "", err, http.StatusBadRequest)
+		return
+	}
+
+	err = overview.CreateTemporaryOverview(userIdentity, version, subject)
+	if err != nil {
+		httpcomm.SetResponseError(&w, "", err, http.StatusInternalServerError)
+		return
+	}
+
+	for {
+		recordList, err := f()
+		if err != nil {
+			httpcomm.SetResponseError(&w, "", err, http.StatusInternalServerError)
+			return
+		}
+
+		if len(recordList) == 0 {
+			break // No more records to insert
+		}
+
+		err = overview.BulkInsertIntoOverview(userIdentity, subject, recordList, true)
+		if err != nil {
+			httpcomm.SetResponseError(&w, "", err, http.StatusInternalServerError)
+			return
+		}
+	}
+
+	err = overview.CommitOverview(userIdentity, subject)
+	if err != nil {
+		httpcomm.SetResponseError(&w, "", err, http.StatusInternalServerError)
+		return
+	}
+
+	httpcomm.ServiceResponse{
+		Data: "OK",
+	}.WriteData(w, httpcomm.PayloadFormatJSON)
+}
