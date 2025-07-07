da91d2604af7207bc9fe54b5f4036d3fe81e6e64
Dmitry Chaykin <d.chaykin@setlog.com>
2025-07-07 14:12:47 +0200

    rebuild overview implemented

--- database/entity.go
+++ database/entity.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 
 	"github.com/dchaykin/go-modules/datamodel"
+	"github.com/dchaykin/go-modules/log"
+	"go.mongodb.org/mongo-driver/bson"
 )
 
 func GetDomainEntityByUUID(uuid string, domainEntity datamodel.DomainEntity) error {
@@ -21,4 +23,36 @@ func GetDomainEntityByUUID(uuid string, domainEntity datamodel.DomainEntity) err
 		return fmt.Errorf("no record with UUID %s found", uuid)
 	}
 	return nil
+}
+
+func ReadDomainEntities(session DatabaseSession, coll Collection, offset, limit int64) ([]datamodel.Record, error) {
+	dataList := []any{}
+	sortOpt := bson.D{{Key: "uuid", Value: 1}}
+	count, err := session.Extract(coll, nil, &dataList, sortOpt, offset, limit)
+	if err != nil {
+		return nil, err
+	}
+	if count == 0 {
+		return nil, nil
+	}
+	resultList, err := convertToDomainEntities(dataList)
+	return resultList, log.WrapError(err)
+}
+
+func convertToDomainEntities(sourceList []any) (resultList []datamodel.Record, err error) {
+	for i, item := range sourceList {
+		o, err := bson.Marshal(item)
+		if err != nil {
+			return nil, err
+		}
+
+		var entity datamodel.Record
+		err = bson.Unmarshal(o, &entity)
+		if err != nil {
+			return nil, fmt.Errorf("failed to unmarshal item %d: %w", i, err)
+		}
+		resultList = append(resultList, entity)
+	}
+
+	return resultList, nil
 }
---
+++ database/entity_test.go
@@ -0,0 +1,39 @@
+package database
+
+import (
+	"testing"
+
+	"github.com/dchaykin/go-modules/helper"
+	"github.com/stretchr/testify/require"
+)
+
+func TestReadDomainEntites(t *testing.T) {
+	helper.LoadAccessData("../.do-not-commit/mongo-test.access")
+
+	session, err := OpenSession()
+	require.NoError(t, err)
+	defer session.Close()
+
+	coll := session.GetCollection("masterData", "location")
+	offset := int64(0)
+
+	entities, err := ReadDomainEntities(session, coll, offset, 3)
+	require.NoError(t, err)
+	require.EqualValues(t, 3, len(entities), "Expected to read 3 entities from the collection.")
+
+	t.Logf("Found %d entities in the first transaction.", len(entities))
+
+	offset += int64(len(entities))
+
+	entities, err = ReadDomainEntities(session, coll, offset, 3)
+	require.NoError(t, err)
+	require.Greater(t, len(entities), 0)
+	t.Logf("Found %d entities in the second transaction.", len(entities))
+
+	offset += int64(len(entities))
+
+	entities, err = ReadDomainEntities(session, coll, offset, 3)
+	require.NoError(t, err)
+	require.EqualValues(t, 0, len(entities))
+	t.Logf("No records to read in the third transaction.")
+}
